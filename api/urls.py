9e93d5d5df94c4fbdb5c9324c0339e413df3d7c1
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-28 16:44:07 -0400

    updated api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views.user import SignUp, SignIn, SignOut, ChangePassword
-from api.views.garden_tasks import Garden_TasksView
+from api.views.garden_tasks import Garden_TasksView, Garden_TaskView
 
 
 urlpatterns = [
@@ -10,5 +10,5 @@ urlpatterns = [
     path('sign-out/', SignOut.as_view(), name='sign-out'),
     path('change-password/', ChangePassword.as_view(), name='change-password'),
     path('garden_tasks/', Garden_TasksView.as_view(), name='garden_tasks'),
-
+    path('garden_tasks/<int:pk>', Garden_TaskView.as_view(), name='garden_task'),
 ]
